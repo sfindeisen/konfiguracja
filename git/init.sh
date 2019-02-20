5994ad396ff6629680ebf6ab2008bbf3a3444d3d
Stanislaw Findeisen <fixpoint.combinator@gmail.com>
2019-02-20 14:15:45 +0100

    fix

--- git/init.sh
+++ git/init.sh
@@ -2,4 +2,4 @@
 
 git config --global user.name  'Stanislaw Findeisen'
 git config --global user.email 'fixpoint.combinator@gmail.com'
-git config --global post.default simple
+git config --global push.default simple
