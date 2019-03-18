c3d3f46a8ef936347d8440a6ebf0d7e7985021b1
Stanislaw Findeisen <fixpoint.combinator@gmail.com>
2019-03-18 16:55:52 +0100

    use vim as git editor

--- git/init.sh
+++ git/init.sh
@@ -3,3 +3,4 @@
 git config --global user.name  'Stanislaw Findeisen'
 git config --global user.email 'fixpoint.combinator@gmail.com'
 git config --global push.default simple
+git config --global core.editor vim
