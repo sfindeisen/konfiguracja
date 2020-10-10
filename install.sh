97828b41241de0c035cb826394dedd2c212e1efc
Stanislaw Findeisen <fixpoint.combinator@gmail.com>
2020-10-10 14:31:27 +0200

    install bash aliases

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ cp -rv "${SCRIPT_DIR}/.stack" "$HOME"
 cp -rv "${SCRIPT_DIR}/.vim"   "$HOME"
 
 cat "$SCRIPT_DIR/.profile" >> "${HOME}/.profile"
+cat "$SCRIPT_DIR/.bash_aliases" >> "${HOME}/.bash_aliases"
 
 "${SCRIPT_DIR}/git/init.sh"
 
