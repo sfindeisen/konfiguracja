173a2027661f1a08c75668a7380ab3003d352764
Stanislaw Findeisen <sfindeisen@users.noreply.github.com>
2022-02-23 21:54:04 +0100

    add gpg.conf

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ SCRIPT_DIR=`dirname "$(readlink -f "$0")"`
 cp -vr "${SCRIPT_DIR}/.config"     "$HOME"
 cp -v  "${SCRIPT_DIR}/.emacs"      "$HOME"
 cp -v  "${SCRIPT_DIR}/.gitconfig"  "$HOME"
+cp -vr "${SCRIPT_DIR}/.gnupg"      "$HOME"
 cp -vr "${SCRIPT_DIR}/.stack"      "$HOME"
 cp -vr "${SCRIPT_DIR}/.vim"        "$HOME"
 
