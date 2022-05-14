22faa16473f566912fe7d85e0208e375958c4dca
Stanislaw Findeisen <sfindeisen@users.noreply.github.com>
2022-05-14 15:34:29 +0200

    fix file copy

--- install.sh
+++ install.sh
@@ -2,13 +2,35 @@
 
 SCRIPT_DIR=`dirname "$(readlink -f "$0")"`
 
-cp -vr "${SCRIPT_DIR}/bin"         "$HOME"
-cp -vr "${SCRIPT_DIR}/.config"     "$HOME"
-cp -v  "${SCRIPT_DIR}/.emacs"      "$HOME"
-cp -v  "${SCRIPT_DIR}/.gitconfig"  "$HOME"
-cp -vr "${SCRIPT_DIR}/.gnupg"      "$HOME"
-cp -vr "${SCRIPT_DIR}/.stack"      "$HOME"
-cp -vr "${SCRIPT_DIR}/.vim"        "$HOME"
+function dircopy() {
+  FROM_DIR=$1
+  TO_DIR=$2
+
+  if [ -d "${TO_DIR}" ] ; then
+    cp -vr ${FROM_DIR}/* "${TO_DIR}/"
+  else
+    cp -vr ${FROM_DIR}   "${TO_DIR}"
+  fi
+}
+
+function install_dir() {
+  INST_DIR=$1
+  dircopy "${SCRIPT_DIR}/${INST_DIR}" "${HOME}/${INST_DIR}"
+}
+
+function install_file() {
+  INST_FILE=$1
+  cp -v "${SCRIPT_DIR}/${INST_FILE}" "${HOME}/"
+}
+
+install_dir  bin
+install_dir .config
+install_dir .gnupg
+install_dir .stack
+install_dir .vim
+
+install_file .emacs
+install_file .gitconfig
 
 cat "$SCRIPT_DIR/.profile" >> "${HOME}/.profile"
 cat "$SCRIPT_DIR/.bash_aliases" >> "${HOME}/.bash_aliases"
