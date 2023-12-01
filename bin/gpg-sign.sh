10ce0ef12d3a35e6bc0d9c1c23af5bc64df80429
Stanislaw Findeisen <stanislaw@ik.me>
2023-12-01 10:44:11 +0100

    implement gpg-sign.sh

---
+++ bin/gpg-sign.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Signs the given file using the default PGP key.
+
+set -o errexit
+set -o noglob
+set -o notify
+set -o pipefail
+
+ARGC=$#
+
+if test $ARGC -ne 1 || test "--help" = "$1" ; then
+  echo "usage: $0 FILE";
+  exit 1;
+fi
+
+FILE="$1"
+
+if [ ! -f "$FILE" ] ; then
+  echo "usage: $0 FILE";
+  exit 2;
+fi
+
+umask 0077
+gpg -a -s "$FILE"
